fb603982651f8b3b1980cd88a3103270f802b06d
Cadreel <cleber.alessandro@globo.com>
2018-11-08 01:38:14 -0200

    testeBD

---
+++ conexao.php
@@ -0,0 +1,9 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "forms";
+
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
---
+++ processa.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once("conexao.php");
+
+$nome = filter_input(INPUT_POST,'nome', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST,'email', FILTER_SANITIZE_EMAIL);
+$plataforma = filter_input(INPUT_POST,'plataforma', FILTER_SANITIZE_STRING);
+$mensagem = filter_input(INPUT_POST,'mensagem', FILTER_SANITIZE_STRING);
+
+
+//echo "Nome: $nome <br>";
+//echo "Email: $email <br>";
+//echo "Obs: $obs <br>";
+//echo "Plataforma: $platforma <br>";
+
+$result_usuario = "INSERT INTO dataform (nome, email, plataforma, mensagem, created) VALUES ('$nome', $email, $plataforma, $mensagem, NOW())";
+
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+
+if (mysqli_insert_id($conn)){
+		header("Location: index.php");
+
+}else{
+		header("Location: index.php");
+
+}
+
