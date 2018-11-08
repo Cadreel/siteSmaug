e51d64d4326c3840dceecb3dd35389be4ac89f7a
Cadreel <cleber.alessandro@globo.com>
2018-11-08 21:47:11 -0200

    testeBD1

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
 
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
+$servidor = "127.0.0.1";
+$usuario = "azure";
+$senha = "6#vWHD_$";
 $dbname = "localdb";
 
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
