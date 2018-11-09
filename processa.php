9b5f07fbdf7e1b48100f041cc4174bf8fbeb0da9
Cadreel <cleber.alessandro@globo.com>
2018-11-09 01:11:47 -0200

    testeBD

--- processa.php
+++ processa.php
@@ -8,22 +8,24 @@ $plataforma = filter_input(INPUT_POST,'plataforma', FILTER_SANITIZE_STRING);
 $mensagem = filter_input(INPUT_POST,'mensagem', FILTER_SANITIZE_STRING);
 
 
-echo "Nome: $nome <br>";
+/*echo "Nome: $nome <br>";
 echo "Email: $email <br>";
 echo "Plataforma: $plataforma <br>";
-echo "Mensagem: $mensagem <br>";
+echo "Mensagem: $mensagem <br>";*/
 
 
 $result_usuario = "INSERT INTO teste_table (nome, email, plataforma, mensagem, created) VALUES ('$nome', '$email', '$plataforma', '$mensagem', NOW())";
 
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 
-if (mysqli_connect()){
+if ($conn->connect_error){
 		echo "Failed: " . mysqli_connect_errno();
 
 }else{
-		echo "Facadinhas: " . $result_usuario;
+		echo "Facadinhas:  . $result_usuario";
 
 }
+$conn->close();
+?>
 
 
