c4cfc53d6d87512a7c1eba43b8b76ed716dfede2
Cadreel <cleber.alessandro@globo.com>
2018-11-09 15:39:02 -0200

    testeBDSucess

--- conexao.php
+++ conexao.php
@@ -5,7 +5,10 @@ $usuario = "azure";
 $senha = "6#vWHD_$";
 $dbname = "localdb";
 
-$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+//$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+$conn=mysqli_init(); [mysqli_ssl_set($conn, NULL, NULL, {ca-cert filename}, NULL, NULL);] 
+mysqli_real_connect($con, $servidor, $usuario, $senha, $dbname, 80);
 
 
 ?>
