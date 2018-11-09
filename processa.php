07ddd459e2fa78e2c6db280cbc8ac5febc3d2c03
Cadreel <cleber.alessandro@globo.com>
2018-11-09 00:32:48 -0200

    testeBD

--- processa.php
+++ processa.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once("conexao.php");
+include_once("./conexao.php");
 
 $nome = filter_input(INPUT_POST,'nome', FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST,'email', FILTER_SANITIZE_EMAIL);
