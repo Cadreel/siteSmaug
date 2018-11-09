9b3f3227592ce643f11f49814aa6b131b542bb9c
Cadreel <cleber.alessandro@globo.com>
2018-11-09 00:54:32 -0200

    testeBD

--- processa.php
+++ processa.php
@@ -8,29 +8,22 @@ $plataforma = filter_input(INPUT_POST,'plataforma', FILTER_SANITIZE_STRING);
 $mensagem = filter_input(INPUT_POST,'mensagem', FILTER_SANITIZE_STRING);
 
 
-/*echo "Nome: $nome <br>";
+echo "Nome: $nome <br>";
 echo "Email: $email <br>";
 echo "Plataforma: $plataforma <br>";
-echo "Mensagem: $mensagem <br>";*/
+echo "Mensagem: $mensagem <br>";
 
 
 $result_usuario = "INSERT INTO teste_table (nome, email, plataforma, mensagem, created) VALUES ('$nome', '$email', '$plataforma', '$mensagem', NOW())";
 
 $resultado_usuario = mysqli_query($conn, $result_usuario);
 
-echo "Facadinhas: $resultado_usuario <br>";
+if (mysqli_connect_errno()){
+		echo "Failed: " . mysqli_connect_errno();
 
-if(mysqli_connect_errno()){
-	echo "Failed: " . mysqli_connect_errno();
 }else{
-	mysqli_query($conn, $result_usuario);
+		header("Location: index.php");
+
 }
-/*if (mysqli_insert_id($conn)){
-		header("Location: index.php");
 
-}else{
-		header("Location: index.php");
 
-}*/
-
-?>
