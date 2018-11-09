477775836154ee5ea3a38e6169deb3dda8856c1e
Cadreel <cleber.alessandro@globo.com>
2018-11-09 00:43:05 -0200

    testeBD

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	</head>
 	<body>
 		<h1>Digue</h1>
-		<form method= "POST" action="processa.php">
+		<form method= "post" action="processa.php">
 			<label>Nome:</label>
 			<input type="text" name="nome" placeholder="Richard Belmont"><br><br>
 			<label>Email:</label>
