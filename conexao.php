d797f21195a7570702e42bf45aab735048836c99
Cadreel <cleber.alessandro@globo.com>
2018-11-09 16:32:12 -0200

    testebd1

--- conexao.php
+++ conexao.php
@@ -1,14 +1,11 @@
 <?php
 
-$servidor = "127.0.0.1:50105";
-$usuario = "azure";
-$senha = "6#vWHD_$";
-$dbname = "localdb";
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "testebd";
 
-//$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-
-$conn=mysqli_init(); [mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);] 
-mysqli_real_connect($conn, $servidor, $usuario, $senha, $dbname, NULL);
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
 
 ?>
