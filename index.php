e613610786e57e397cc9fce67965545151c073d1
Cadreel <cleber.alessandro@globo.com>
2018-11-11 15:01:43 -0200

    teste

--- index.php
+++ index.php
@@ -22,5 +22,56 @@
 			
 			<input type="submit" value="Enviar">
 		</form>
+		<?php
+		header('Content-Type: text/html; charset=utf-8');
+        date_default_timezone_set('America/Sao_Paulo');
+        try{
+            $conn = new PDO("mysql:host=$DB_HOST; dbname=$DB_DATABASE", $DB_USER, $DB_PASSWORD);
+            $conn->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(Exception $e){
+            die(var_dump($e));
+        }
+        if(!empty($_POST)){
+            try{
+                $name = $_POST['nome'];
+				$email = $_POST['email'];
+				$plataforma = $_POST['plataforma'];
+				$mensagem = $_POST['mensagem'];
+				$data = date("Y-m-d");
+
+				$sql_insert = "INSERT INTO testebd (nome, email, plataforma, mensagem, data) VALUES (:nome, :email, :plataforma, :mensagem, :data)";
+				$stmt = $conn->prepare($sql_insert);
+				$stmt->execute(array(':nome' => $nome, ':email' => $email, ':plataforma' => $plataforma, ':mensagem' => $mensagem, ':data' => $data));
+			}catch(Exception $e){
+				die(var_dump $e);
+			}
+			echo "<h3> Você inseriu o contato</h3>";
+		}
+
+		$stmt = $conn->query('SELECT * FROM testebd');
+		$testebd = $stmt->fetchAll();
+
+		if(count($testebd)){
+			echo "<h2> Pessoas que colocaram o contato:</h2>";
+			echo "<table>";
+			echo "<tr><th>Nome</th>";
+			echo "<tr><th>Email</th>";
+			echo "<tr><th>Plataforma</th>";
+			echo "<tr><th>Mensagem</th>";
+			echo "<tr><th>Data</th></tr>";
+
+			foreach ($testebd as $testebd1) {
+				echo "<tr><td>".$testebd1['nome']."</td>";
+				echo "<td>".$testebd1['email']."</td>";
+				echo "<td>".$testebd1['plataforma']."</td>";
+				echo "<td>".$testebd1['mensagem']."</td>";
+				echo "<td>".$testebd1['data']."</td></tr>";
+			}
+			echo "</table>";
+		}else{
+			echo "<h3>Não tem nenhum contato</h3>"
+		}
+		?>
+			
 	</body>
 </html>
